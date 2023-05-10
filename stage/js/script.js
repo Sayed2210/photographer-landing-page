4628bff3d972796a43ce99091dd51b28fd8f9dac
Elsayed Hassan <eh600039@gmail.com>
2023-05-10 19:44:54 +0300

    40%

---
+++ stage/js/script.js
@@ -0,0 +1,15 @@
+AOS.init();
+// $(document).ready(function(){
+//     $('.owl-one').owlCarousel({
+//         animateOut: 'slideOutDown',
+//         animateIn: 'flipInX',
+//         items:1,
+//         margin:30,
+//         loop:true,
+//         stagePadding:30,
+//         smartSpeed:450,
+//         autoplay:true,
+//         autoplayTimeout:1000,
+//         autoplayHoverPause:true
+//     });
+//   });
